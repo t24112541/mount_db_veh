8bd13258a18d7b99a1ffa37748c711b4ca7ede7b
t24112541 <t24112541@gmail.com>
2019-04-02 12:09:29 +0700

    chk

--- connect/connect_bridge.php
+++ connect/connect_bridge.php
@@ -2,6 +2,12 @@
 /// -------------------  host=ต้นทาง ---------------------------
 	$que_host=mysqli_query($my_conn,"select * from my_mount,my_match where my_mount.my_id=my_match.m_host_id");
 	$sh_host=mysqli_fetch_array($que_host);
+	echo $sh_host['my_host']."<br>";
+	echo $sh_host['my_user']."<br>";
+	echo $sh_host['my_pass']."<br>";
+	echo $sh_host['my_name']."<br>";
+	echo $sh_host['my_port']."<br>";
+
 	$host_db=mysqli_connect($sh_host['my_host'],$sh_host['my_user'],$sh_host['my_pass'],$sh_host['my_name'],$sh_host['my_port']) or die("connect host_db error");
 	$host_db->set_charset("utf8");
 	if(!$host_db){echo "please check syntax on host_db";}
